e85a447b5fad32ddd4308ebcc0a831c285e60bb9
Harin3Bone <harin.thananam@gmail.com>
2021-02-15 22:26:08 +0700

    Controller - add @GetMapping with Query param

--- src/main/java/com/tutorial/springboot/controller/GetController.java
+++ src/main/java/com/tutorial/springboot/controller/GetController.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -18,5 +19,10 @@ public class GetController {
 	@GetMapping(value = "/path/{path}")
 	public ResponseEntity<Object> getMappingWithPath(@PathVariable(name = "path") String pathParam) {
 		return ResponseEntity.status(HttpStatus.OK).body("GetMapping [PathParam] success");
+	}
+
+	@GetMapping(value = "query")
+	public ResponseEntity<Object> getMappingWithQuery(@RequestParam(name = "query") String queryParam) {
+		return ResponseEntity.status(HttpStatus.OK).body("GetMapping [QueryParam] success");
 	}
 }

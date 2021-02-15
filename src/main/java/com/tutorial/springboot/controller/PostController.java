192eb60a39aa15a60edb2d4ccc86b2028b3aac9a
Harin3Bone <harin.thananam@gmail.com>
2021-02-15 23:08:30 +0700

    Controller - add @PostMapping with request body success

--- src/main/java/com/tutorial/springboot/controller/PostController.java
+++ src/main/java/com/tutorial/springboot/controller/PostController.java
@@ -1,17 +1,32 @@
 package com.tutorial.springboot.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PostController {
 
 	@PostMapping(value = "/simple")
-	public ResponseEntity<Object> postMapping(){
+	public ResponseEntity<Object> postMapping() {
 		System.out.println("PostMapping success");
 		return ResponseEntity.status(HttpStatus.CREATED).body("PostMapping success");
-	}	
+	}
+
+	@PostMapping(value = "/body/obj")
+	public ResponseEntity<Object> postMappingWithObj(@RequestBody Map<String, Object> body) {
+		System.out.println("PostMapping with JSONObject");
+		return ResponseEntity.status(HttpStatus.CREATED).body("PostMapping with JSONObject, Body = " + body);
+	}
 	
+	@PostMapping(value = "/body/arr")
+	public ResponseEntity<Object> postMappingWithArr(@RequestBody List<Object> body){
+		System.out.println("PostMapping with JSONArray");
+		return ResponseEntity.status(HttpStatus.CREATED).body("PostMapping with JSONArray, Body = " + body);
+	}
 }

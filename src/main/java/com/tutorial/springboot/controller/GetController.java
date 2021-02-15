b38e916aa8ee80a022f544b3fa5554efb0c9b694
Harin3Bone <harin.thananam@gmail.com>
2021-02-15 22:21:59 +0700

    Controller - add @GetMapping with Path param

--- src/main/java/com/tutorial/springboot/controller/GetController.java
+++ src/main/java/com/tutorial/springboot/controller/GetController.java
@@ -3,6 +3,7 @@ package com.tutorial.springboot.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +13,10 @@ public class GetController {
 	public ResponseEntity<Object> getMapping() {
 		System.out.println("GetMapping");
 		return ResponseEntity.status(HttpStatus.OK).body("GetMapping success");
+	}
+
+	@GetMapping(value = "/path/{path}")
+	public ResponseEntity<Object> getMappingWithPath(@PathVariable(name = "path") String pathParam) {
+		return ResponseEntity.status(HttpStatus.OK).body("GetMapping [PathParam] success");
 	}
 }

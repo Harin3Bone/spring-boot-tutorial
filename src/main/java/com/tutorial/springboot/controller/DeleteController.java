f7006e4545bc943e41e0302082706d792600e62c
Harin3Bone <harin.thananam@gmail.com>
2021-02-17 12:09:56 +0700

    Controller - add another way for set query params in @DeleteMapping

--- src/main/java/com/tutorial/springboot/controller/DeleteController.java
+++ src/main/java/com/tutorial/springboot/controller/DeleteController.java
@@ -3,6 +3,7 @@ package com.tutorial.springboot.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +16,8 @@ public class DeleteController {
 	}
 	
 	@DeleteMapping(value = "/path",params = "data")
-	public ResponseEntity<Object> deleteMappingWithParam(){
+	public ResponseEntity<Object> deleteMappingWithParam(@RequestParam("data") String data){
 		System.out.println("DeleteMapping with Param success");
-		return ResponseEntity.status(HttpStatus.OK).body("DeleteMapping with Param success");
+		return ResponseEntity.status(HttpStatus.OK).body("DeleteMapping with Param success, data= " + data);
 	}
 }

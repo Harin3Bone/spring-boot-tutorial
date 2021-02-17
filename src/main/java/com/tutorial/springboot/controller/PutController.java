7a69773315cf4f777daf0622b00b44b2d9581c92
Harin3Bone <harin.thananam@gmail.com>
2021-02-17 12:16:09 +0700

    Controller - add @apiNote in @PutMapping&@DeleteMapping

--- src/main/java/com/tutorial/springboot/controller/PutController.java
+++ src/main/java/com/tutorial/springboot/controller/PutController.java
@@ -9,15 +9,30 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * @author Harin Thananam
+ * @since 17 FEB 2021
+ * @category PUT Example
+ * */
+
 @RestController
 public class PutController {
 
+	/**
+	 * @apiNote http://localhost:3000/simple
+	 * @response text/plain
+	 */
 	@PutMapping(value = "/simple")
 	public ResponseEntity<Object> putMapping() {
 		System.out.println("PutMapping success");
 		return ResponseEntity.status(HttpStatus.OK).body("PutMapping success");
 	}
 
+	/**
+	 * @apiNote http://localhost:3000/simple
+	 * @body application/json {}
+	 * @response application/json
+	 */
 	@PutMapping(value = "/body", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> putMappingWithBody(@RequestBody Map<String, Object> body) {
 		System.out.println("PutMapping with Body");

265cab00b61d592ea0c8a19f6ea264cff4ac33b8
Harin3Bone <harin.thananam@gmail.com>
2021-02-24 22:34:25 +0700

    OpenApi - Implement POST document

--- src/main/java/com/tutorial/springboot/controller/StudentController.java
+++ src/main/java/com/tutorial/springboot/controller/StudentController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tutorial.springboot.entity.StudentEntity;
@@ -25,6 +24,7 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -45,10 +45,7 @@ public class StudentController {
 
 	@Operation(summary = "Get all student")
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200",content = @Content(
-					mediaType = MediaType.APPLICATION_JSON_VALUE,
-					schema = @Schema(implementation = TemplateResModel.class)))
-	})
+			@ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TemplateResModel.class))) })
 	@GetMapping(value = "/students")
 	public ResponseEntity<Object> getAllStudent() {
 		List<StudentEntity> data = studentService.getAllStudent();
@@ -60,14 +57,9 @@ public class StudentController {
 	}
 
 	@Operation(summary = "Get student by Id")
-	@Parameters(value = {
-			@Parameter(name = "id",required = true,allowEmptyValue = false,example = "1")
-	})
+	@Parameters(value = { @Parameter(name = "id", required = true, allowEmptyValue = false, example = "1") })
 	@ApiResponses(value = {
-			@ApiResponse(responseCode = "200",content = @Content(
-					mediaType = MediaType.APPLICATION_JSON_VALUE,
-					schema = @Schema(implementation = TemplateResModel.class)))
-	})
+			@ApiResponse(responseCode = "200", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TemplateResModel.class))) })
 	@GetMapping(value = "/student/{id}")
 	public ResponseEntity<Object> getStudentById(@PathVariable("id") long id) {
 		StudentEntity data = studentService.getStudentById(id);
@@ -78,6 +70,10 @@ public class StudentController {
 		return ResponseEntity.status(status).body(res);
 	}
 
+	@Operation(summary = "Create once student")
+	@RequestBody(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = StudentEntity.class)), description = "Student data")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TemplateResModel.class))) })
 	@PostMapping(value = "/student", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Object> saveStudent(@RequestBody Map<String, Object> body) {
 		StudentEntity data = studentService.saveStudent(body);
@@ -88,9 +84,13 @@ public class StudentController {
 		return ResponseEntity.status(status).body(res);
 	}
 
+	@Operation(summary = "Create list of student")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = TemplateResModel.class))) })
 	@PostMapping(value = "/students", consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<Object> saveStudentList(@RequestBody List<Map<String, Object>> body) {
-		List<StudentEntity> data = studentService.saveStudentList(body);
+	public ResponseEntity<Object> saveStudentList(
+			@RequestBody(content = @Content(mediaType = MediaType.APPLICATION_JSON_VALUE), description = "List of student data") List<StudentEntity> body) {
+		List<StudentEntity> data = studentService.saveStudentListv2(body);
 		String message = "Create student success " + data.size() + " row(s)";
 		HttpStatus status = HttpStatus.CREATED;
 
@@ -108,13 +108,13 @@ public class StudentController {
 		TemplateResModel res = new TemplateResModel(status, message, data);
 		return ResponseEntity.status(status).body(res);
 	}
-	
+
 	@DeleteMapping(value = "/student/{id}")
-	public ResponseEntity<Object> deleteStudentById(@PathVariable("id") long id){
+	public ResponseEntity<Object> deleteStudentById(@PathVariable("id") long id) {
 		studentService.deleteStudent(id);
 		String message = "Delete id " + id + " success";
 		HttpStatus status = HttpStatus.OK;
-		
+
 		TemplateResModel res = new TemplateResModel(status, message, "");
 		return ResponseEntity.status(status).body(res);
 	}

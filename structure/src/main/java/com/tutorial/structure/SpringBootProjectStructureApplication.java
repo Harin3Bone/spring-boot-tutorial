d66a4974e30fd9d26e73fdccab65abdfa2676891
harin <ace.harin1@gmail.com>
2021-04-16 11:30:44 +0700

    Structure - create basic structure

---
+++ structure/src/main/java/com/tutorial/structure/SpringBootProjectStructureApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectStructureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProjectStructureApplication.class, args);
+	}
+
+}

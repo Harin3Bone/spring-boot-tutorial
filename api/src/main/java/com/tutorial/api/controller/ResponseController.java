582f99d28a9ef955ba000346d514195f914df8a1
Harin3Bone <harin.thananam@gmail.com>
2021-07-21 21:47:33 +0700

    JSON - change request mapping value

--- api/src/main/java/com/tutorial/api/controller/ResponseController.java
+++ api/src/main/java/com/tutorial/api/controller/ResponseController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tutorial.api.entity.ResponseModel;
 
 @RestController
-@RequestMapping(value = "/json")
+@RequestMapping(value = "/res")
 @CrossOrigin(allowedHeaders = "*", origins = "*")
 public class ResponseController {
 

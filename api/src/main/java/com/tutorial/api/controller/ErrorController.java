d653c385092abcd014f68e5ae3fda56758e5a036
Harin3Bone <harin.thananam@gmail.com>
2021-07-21 21:38:21 +0700

    Exception - add more tutorial of error handle

--- api/src/main/java/com/tutorial/api/controller/ErrorController.java
+++ api/src/main/java/com/tutorial/api/controller/ErrorController.java
@@ -1,5 +1,7 @@
 package com.tutorial.api.controller;
 
+import java.util.HashMap;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -7,14 +9,29 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tutorial.api.entity.ResponseModel;
+
 @RestController
 @RequestMapping(value = "/err")
-@CrossOrigin(allowedHeaders = "*" , origins = "*")
+@CrossOrigin(allowedHeaders = "*", origins = "*")
 public class ErrorController {
 
-	@GetMapping(value = "")
-	public ResponseEntity<Object> exampleError(){
-		var num = Integer.parseInt("XXX");
+	@GetMapping(value = "nocatch")
+	public ResponseEntity<Object> exampleError() {
+		var num = Integer.parseInt("Exception");
 		return ResponseEntity.status(HttpStatus.OK).body(num);
+	}
+
+	@GetMapping(value = "catch")
+	public ResponseModel exampleHandleError() {
+		try {
+			var mp = new HashMap<String, Object>();
+			var res = mp.get("value").toString();
+			return new ResponseModel(res);
+		} catch (NullPointerException e) {
+			throw e;
+		} catch (Exception e) {
+			throw e;
+		}
 	}
 }

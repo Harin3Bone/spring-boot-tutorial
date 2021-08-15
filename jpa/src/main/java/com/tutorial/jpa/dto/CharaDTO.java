b54c5aeee0a7a33d68e2dcd94dd610c7cffb017e
Harin3Bone <harin.thananam@gmail.com>
2021-08-15 14:20:42 +0700

    JPA - update jpa structure and refactor as sonarLint

---
+++ jpa/src/main/java/com/tutorial/jpa/dto/CharaDTO.java
@@ -0,0 +1,7 @@
+package com.tutorial.jpa.dto;
+
+import com.tutorial.jpa.entity.CharaEntity;
+
+public class CharaDTO extends CharaEntity{
+
+}
--- jpa/src/main/java/com/tutorial/jpa/service/CharaService.java
+++ jpa/src/main/java/com/tutorial/jpa/service/CharaService.java
@@ -2,77 +2,19 @@ package com.tutorial.jpa.service;
 
 import java.util.List;
 
-import javax.annotation.Resource;
-
-import org.springframework.stereotype.Service;
-
 import com.tutorial.jpa.entity.CharaEntity;
-import com.tutorial.jpa.repository.CharaRepository;
 
-@Service
-public class CharaService {
+public interface CharaService {
 
-	@Resource
-	private CharaRepository charaRepo;
-
-	public List<CharaEntity> findAllChara() {
-		try {
-			return charaRepo.findAll();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public CharaEntity findCharaById(long id) {
-		try {
-			return charaRepo.findById(id).get();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void createChara(CharaEntity chara) {
-		try {
-			charaRepo.save(chara);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void updateChara(CharaEntity chara) {
-		try {
-			CharaEntity defChara = charaRepo.findById(chara.getId()).get();
-
-			defChara.setName(chara.getName());
-			defChara.setClss(chara.getClss());
-			defChara.setLabel(chara.getLabel());
-			defChara.setLv(chara.getLv());
-			
-			charaRepo.save(defChara);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
+	public List<CharaEntity> findAllChara();
 	
-	public void deleteAllChara() {
-		try {
-			charaRepo.deleteAll();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	public void deleteCharaById(long id) {
-		try {
-			charaRepo.deleteById(id);
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
+	public CharaEntity findCharaById(long id);
+	
+	public void createChara(CharaEntity chara);
+	
+	public void updateChara(CharaEntity chara);
+	
+	public void deleteAllChara();
+	
+	public void deleteCharaById(long id);
 }

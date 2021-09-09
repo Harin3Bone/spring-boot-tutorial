82f68800b197b69bdff5c15806afa4b4677c43ec
Harin3Bone <harin.thananam@gmail.com>
2021-09-10 00:21:38 +0700

    Exception - handle default exception

--- exception/src/main/java/com/tutorial/handler/helper/InternalHandler.java
+++ exception/src/main/java/com/tutorial/handler/helper/InternalHandler.java
@@ -1,8 +1,23 @@
 package com.tutorial.handler.helper;
 
+import java.util.HashMap;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.tutorial.handler.model.ResponseModel;
 
 @RestControllerAdvice
 public class InternalHandler {
 
+	@ExceptionHandler(value = Exception.class)
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+	public ResponseModel handleMediaTypeNotSupport(Exception e) {
+		var res = new HashMap<Integer, String>();
+		res.put(0, "Unexpect error exception.");
+
+		return new ResponseModel(e.getClass().getSimpleName(), res);
+	}
 }

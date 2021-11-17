c1eb278bb4b49be9f2b6eca76a93884c1ca497c1
Harin3Bone <harin.thananam@gmail.com>
2021-11-18 00:05:14 +0700

    WebSocket - create starter websocket success

--- websocket/src/main/java/com/tutorial/websocket/SpringBootWebsocketApplication.java
+++ websocket/src/main/java/com/tutorial/websocket/SpringBootWebsocketApplication.java
@@ -2,8 +2,10 @@ package com.tutorial.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class SpringBootWebsocketApplication {
 
 	public static void main(String[] args) {
---
+++ websocket/src/main/java/com/tutorial/websocket/message/MessageCaller.java
@@ -0,0 +1,10 @@
+package com.tutorial.websocket.message;
+
+import lombok.Data;
+
+@Data
+public class MessageCaller {
+
+	private String from;
+	private String text;
+}
